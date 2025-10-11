149072e60f6934282039353e466627fec4d7744d
Menghao <32590119+burgerman@users.noreply.github.com>
2025-10-11 00:26:23 -0400

    Update api_key var

--- genai_scripts/model_list_check.py
+++ genai_scripts/model_list_check.py
@@ -1,8 +1,7 @@
 from google import genai
 from google.genai import types
 
-API_KEY = "AIzaSyDK19UnJoC283B3w-_kaa1o9CfoExaJrbg"   # replace with your API key
-# API_KEY = "AIzaSyACH8BV1h1WscmMcbv6wXygmpBm_Z--yIE"
+API_KEY = ""   # replace with your API key
 
 client = genai.Client(api_key=API_KEY)
 for model in client.models.list():
